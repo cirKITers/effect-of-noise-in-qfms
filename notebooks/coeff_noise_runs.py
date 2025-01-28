bd5d251bbe0bf5fcea7053580a7bb37f7593f3b9
Melvin Strobl <lc3267@kit.edu>
2025-01-28 17:28:00 +0100

    switched back to scatter plots
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- notebooks/coeff_noise_runs.py
+++ notebooks/coeff_noise_runs.py
@@ -1,4 +1,5 @@
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import plotly.io as pio
 from runs.coefficient_runs import run_ids, experiment_id
 from helper import (
@@ -19,9 +20,10 @@ coeffs_df = assign_ansatz_id(coeffs_df)
 ansaetze = coeffs_df.ansatz.unique()
 qubits = sorted(coeffs_df.qubits.unique())
 
-for metric in ["coeffs_abs_mean", "coeffs_abs_var"]:
-    for ansatz in ansaetze:
-        fig = go.Figure()
+# for metric in ["coeffs_abs_mean", "coeffs_abs_var"]:
+for ansatz in ansaetze:
+    for qubit in qubits:
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
         main_colors_it, sec_colors_it = get_color_iterator()
         for noise in [
             "BitFlip",
@@ -33,66 +35,129 @@ for metric in ["coeffs_abs_mean", "coeffs_abs_var"]:
             main_color_sel = next(main_colors_it)
             sec_color_sel = rgb_to_rgba(next(sec_colors_it), 0.2)
 
-            # metric_values_grid = pd.DataFrame(columns=[qubit for qubit in qubits])
-            for qubit in qubits:
-                metric_values = (
-                    coeffs_df[
-                        (coeffs_df.ansatz == ansatz) & (coeffs_df.qubits == qubit)
-                    ]
-                    .groupby(noise)[metric]
-                    .agg(["mean", "min", "max"])
-                ).iloc[
-                    1:
-                ]  # Don't show noise level 0
+            # # metric_values_grid = pd.DataFrame(columns=[qubit for qubit in qubits])
+            # metric_values = (
+            #     coeffs_df[
+            #         (coeffs_df.ansatz == ansatz) & (coeffs_df.qubits == qubit)
+            #     ]
+            #     .groupby(noise)[metric]
+            #     .agg(["mean", "min", "max"])
+            # ).iloc[
+            #     1:
+            # ]  # Don't show noise level 0
 
-                metric_values_grid[qubit] = metric_values["mean"]
+            # metric_values_grid[qubit] = metric_values["mean"]
 
-            fig.add_surface(
-                # x=metric_values.index,
-                # y=qubits,
-                z=metric_values_grid,
-                name=f"{noise}-{ansatz}",
+            # fig.add_surface(
+            #     # x=metric_values.index,
+            #     # y=qubits,
+            #     z=metric_values_grid,
+            #     name=f"{noise}-{ansatz}",
+            # )
+
+            coeff_mean_metric_values = (
+                coeffs_df[(coeffs_df.ansatz == ansatz) & (coeffs_df.qubits == qubit)]
+                .groupby(noise)["coeffs_abs_mean"]
+                .agg(["mean", "min", "max"])
+            ).iloc[
+                1:
+            ]  # Don't show noise level 0
+
+            coeff_var_metric_values = (
+                coeffs_df[(coeffs_df.ansatz == ansatz) & (coeffs_df.qubits == qubit)]
+                .groupby(noise)["coeffs_abs_var"]
+                .agg(["mean", "min", "max"])
+            ).iloc[
+                1:
+            ]  # Don't show noise level 0
+
+            fig.add_trace(
+                go.Scatter(
+                    x=coeff_mean_metric_values.index,
+                    y=coeff_mean_metric_values["mean"],
+                    name=f"{noise} Mean",
+                    mode="lines",
+                    line=dict(color=main_color_sel),
+                    marker=dict(color=main_color_sel),
+                )
             )
-            # fig.add_trace(
-            #     go.Scatter(
-            #         x=metric_values.index,
-            #         y=metric_values["mean"],
-            #         name=f"{noise}",
-            #         mode="lines",
-            #         line=dict(color=main_color_sel),
-            #         marker=dict(color=main_color_sel),
-            #     )
-            # )
-            # fig.add_trace(
-            #     go.Scatter(
-            #         x=metric_values.index,
-            #         y=metric_values["max"],
-            #         name=f"upper-{noise}",
-            #         visible=True,
-            #         mode="lines",
-            #         line=dict(width=0),
-            #         showlegend=False,
-            #     )
-            # )
-            # fig.add_trace(
-            #     go.Scatter(
-            #         x=metric_values.index,
-            #         y=metric_values["min"],
-            #         name=f"lower-{noise}",
-            #         visible=True,
-            #         mode="lines",
-            #         fill="tonexty",
-            #         fillcolor=sec_color_sel,
-            #         marker=dict(color=main_color_sel),
-            #         line=dict(width=0),
-            #         showlegend=False,
-            #     )
-            # )
+            fig.add_trace(
+                go.Scatter(
+                    x=coeff_mean_metric_values.index,
+                    y=coeff_mean_metric_values["max"],
+                    name=f"upper-{noise}",
+                    visible=True,
+                    mode="lines",
+                    line=dict(width=0),
+                    showlegend=False,
+                )
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=coeff_mean_metric_values.index,
+                    y=coeff_mean_metric_values["min"],
+                    name=f"lower-{noise}",
+                    visible=True,
+                    mode="lines",
+                    fill="tonexty",
+                    fillcolor=sec_color_sel,
+                    marker=dict(color=main_color_sel),
+                    line=dict(width=0),
+                    showlegend=False,
+                )
+            )
 
-        title = metric.replace("_", " ").title()
+            fig.add_trace(
+                go.Scatter(
+                    x=coeff_var_metric_values.index,
+                    y=coeff_var_metric_values["mean"],
+                    name=f"{noise} Var.",
+                    mode="lines",
+                    line=dict(
+                        color=main_color_sel,
+                        dash="dot",
+                    ),
+                    marker=dict(color=main_color_sel),
+                ),
+                secondary_y=True,
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=coeff_var_metric_values.index,
+                    y=coeff_var_metric_values["max"],
+                    name=f"upper-{noise}",
+                    visible=True,
+                    mode="lines",
+                    line=dict(width=0),
+                    showlegend=False,
+                ),
+                secondary_y=True,
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=coeff_var_metric_values.index,
+                    y=coeff_var_metric_values["min"],
+                    name=f"lower-{noise}",
+                    visible=True,
+                    mode="lines",
+                    fill="tonexty",
+                    fillcolor=sec_color_sel,
+                    marker=dict(color=main_color_sel),
+                    line=dict(width=0),
+                    showlegend=False,
+                ),
+                secondary_y=True,
+            )
+
+        fig.update_yaxes(title_text="Coefficient Mean", secondary_y=False)
+        fig.update_yaxes(title_text="Coefficient Variance", secondary_y=True)
+        fig.update_xaxes(title_text="Noise Level")
         fig.update_layout(
-            title=f"{title} over Noise Level",
+            title=f"{ansatz} - {qubit} Qubits - Coefficient Mean and Variance over Noise Level",
             template="plotly_white",
+            legend=dict(x=1.2),
+            width=900,
         )
-        fig.show()
-        # save_fig(fig, f"{metric}_{ansatz}_noise_level", run_ids, experiment_id)
+        save_fig(
+            fig, f"coeff_mean_var_{ansatz}_{qubit}_noise_level", run_ids, experiment_id
+        )
