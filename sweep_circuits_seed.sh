bfb8496b8b7e98e9006c646cc9540f3da56000c8
stroblme <melvin.strobl@kit.edu>
2025-02-21 10:51:03 +0100

    all ansatzes
    Signed-off-by: stroblme <melvin.strobl@kit.edu>

--- sweep_circuits_seed.sh
+++ sweep_circuits_seed.sh
@@ -3,9 +3,10 @@
 # first argument: seed
 
 # run experiments with all different circuits
-for circuit in Hardware_Efficient
+# Hardware_Efficient Circuit_19 Strongly_Entangling Circuit_15
+for circuit in Hardware_Efficient Circuit_19 Strongly_Entangling Circuit_15
 do
     echo "Running with Ansatz $circuit"
 
     ./sweep_noise_circuits_seed.sh $circuit $1
-done
+done    
