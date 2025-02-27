d4d90690e23e6a5467b3624f213894191c25592c
stroblme <melvin.strobl@kit.edu>
2025-02-27 17:15:21 +0100

    circuit sweep
    Signed-off-by: stroblme <melvin.strobl@kit.edu>

--- sweep_circuits_seed.sh
+++ sweep_circuits_seed.sh
@@ -8,6 +8,6 @@ for circuit in Hardware_Efficient Circuit_19 Strongly_Entangling Circuit_15
 do
     echo "Running with Ansatz $circuit"
 
-    # ./sweep_noise_circuits_seed.sh $circuit $1
-    ./sweep_qubits_noise_circuits_seed.sh "GateError" $circuit $1
+    ./sweep_noise_circuits_seed.sh $circuit $1
+    # ./sweep_qubits_noise_circuits_seed.sh "GateError" $circuit $1
 done    
