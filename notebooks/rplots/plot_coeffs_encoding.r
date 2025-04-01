a030d3c2c81e106145e2b82beab0bff74a08bd6b
majafranz <majafranz99@gmail.com>
2025-04-01 17:32:42 +0200

    revers order in plot coeff

--- notebooks/rplots/plot_coeffs_encoding.r
+++ notebooks/rplots/plot_coeffs_encoding.r
@@ -32,6 +32,9 @@ d_coeffs$ansatz <- factor(d_coeffs$ansatz,
     labels = c("SEA", "SEA+", "HEA", "Circuit 15", "Circuit 19")
 )
 d_coeffs$frequency <- as.factor(d_coeffs$freq1)
+if (use_tikz) {
+    d_coeffs$encoding <- factor(d_coeffs$encoding, labels = c("$R_x$", "$R_y$", "$R_z$"))
+}
 
 d_coeffs <- d_coeffs %>%
     mutate(
@@ -46,7 +49,7 @@ d_coeffs <- d_coeffs %>% distinct(ansatz, qubits, frequency, seed, encoding, sam
 
 g <- ggplot(d_coeffs %>% filter(qubits == 6), aes(x = coeffs_full_real, y = coeffs_full_imag, colour = frequency)) +
     geom_point_rast(size = POINT.SIZE, alpha = 0.7, shape = 16, raster.dpi = 600) +
-    facet_nested(ansatz ~ encoding,
+    facet_nested(encoding ~ ansatz,
         labeller = labeller(
             frequency = frequencies_labeller,
             qubits = qubit_labeller,
@@ -68,32 +71,44 @@ d_coeffs <- d_coeffs %>%
     summarise(
         mean_coeff = mean(coeffs_abs),
         sd_coeff = sd(coeffs_abs)
+    ) %>%
+    mutate(
+        upper_bound = mean_coeff + sd_coeff,
+        lower_bound = mean_coeff - sd_coeff,
+        relative_sd = mean_coeff * sd_coeff,
     )
 
-g <- ggplot(d_coeffs, aes(x = freq1, y = mean_coeff, colour = as.factor(qubits))) +
-    geom_point(size = POINT.SIZE) +
+d_coeffs$qubits <- factor(d_coeffs$qubits, levels = c(6, 5, 4, 3))
+d_coeffs <- d_coeffs %>% arrange(qubits)
+
+g <- ggplot(d_coeffs, aes(x = freq1, y = mean_coeff, colour = qubits)) +
     geom_line(linewidth = LINE.SIZE) +
-    facet_nested(ansatz ~ encoding,
+    geom_point(size = POINT.SIZE) +
+    facet_nested(encoding ~ ansatz,
         labeller = labeller(
             frequency = frequencies_labeller,
             qubits = qubit_labeller,
         ),
     ) +
     scale_x_continuous("Frequency") +
-    scale_y_log10(ifelse(use_tikz, "$\\bar{\\lvert{c}_\\omega\\rvert}$ [log]", "|c| Mean [log]"),
+    scale_y_log10(ifelse(use_tikz, "$\\lvert{\\bar{c}}_\\omega\\rvert$ [log]", "|c| Mean [log]"),
         breaks = scales::trans_breaks("log10", function(x) 10^x),
         labels = trans_format("log10", math_format(10^.x))
     ) +
-    scale_colour_manual(ifelse(use_tikz, "\\# Qubits", "# Qubits"), values = COLOURS.LIST) +
-    theme_paper()
+    scale_colour_manual(ifelse(use_tikz, "\\# Qubits", "# Qubits"), values = rev(head(COLOURS.LIST, 4))) +
+    theme_paper() +
+    theme(
+        legend.margin = margin(b = -4, t = 0)
+    ) +
+    guides(colour = guide_legend(reverse = T))
 
 save_name <- str_c("coeff_mean_encoding")
-create_plot(g, save_name, COLWIDTH, 0.3 * HEIGHT)
+create_plot(g, save_name, COLWIDTH, 0.2 * HEIGHT)
 
-g <- ggplot(d_coeffs, aes(x = freq1, y = sd_coeff, colour = as.factor(qubits))) +
+g <- ggplot(d_coeffs, aes(x = freq1, y = sd_coeff, colour = qubits)) +
     geom_point(size = POINT.SIZE) +
     geom_line(linewidth = LINE.SIZE) +
-    facet_nested(ansatz ~ encoding,
+    facet_nested(encoding ~ ansatz,
         labeller = labeller(
             frequency = frequencies_labeller,
             qubits = qubit_labeller,
@@ -102,9 +117,14 @@ g <- ggplot(d_coeffs, aes(x = freq1, y = sd_coeff, colour = as.factor(qubits))) 
     scale_x_continuous("Frequency") +
     scale_y_log10(ifelse(use_tikz, "$\\sigma(\\lvert{c}\\rvert)$ [log]", "|c| Standard Deviation [log]"),
         breaks = scales::trans_breaks("log10", function(x) 10^x),
-        labels = trans_format("log10", math_format(10^.x))) +
-    scale_colour_manual(ifelse(use_tikz, "\\# Qubits", "# Qubits"), values = COLOURS.LIST) +
-    theme_paper()
+        labels = trans_format("log10", math_format(10^.x))
+    ) +
+    scale_colour_manual(ifelse(use_tikz, "\\# Qubits", "# Qubits"), values = rev(head(COLOURS.LIST, 4))) +
+    theme_paper() +
+    theme(
+        legend.margin = margin(b = -4, t = 0)
+    ) +
+    guides(colour = guide_legend(reverse = T))
 
 save_name <- str_c("coeff_sd_encoding")
-create_plot(g, save_name, COLWIDTH, 0.3 * HEIGHT)
+create_plot(g, save_name, COLWIDTH, 0.2 * HEIGHT)
