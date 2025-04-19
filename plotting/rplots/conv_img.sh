cee4a3136f1dc936a0e2fa32396e6f32737a57ed
Melvin Strobl <lc3267@kit.edu>
2025-04-19 17:06:52 +0200

    updated images
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- plotting/rplots/conv_img.sh
+++ plotting/rplots/conv_img.sh
@@ -2,4 +2,4 @@ for file in img-gen/*.pdf; do
     echo "$file>${file%.pdf}.png"
     magick -density 400 "$file" -quality 90 "${file%.pdf}_light.png"
 done
-mv img-gen/*.png ../../docs/figures/
+mv ./img-gen/*.png ../../docs/figures/
