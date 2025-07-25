d79075450f782ccc78a155dcfdeeae7b274ed013
majafranz <majafranz99@gmail.com>
2025-07-25 15:06:43 +0200

    zero coeff AD

--- plotting/rplots/plot_coeffs.r
+++ plotting/rplots/plot_coeffs.r
@@ -111,7 +111,7 @@ g <- ggplot(d_coeffs_ansatz %>% filter(noise_type %in% c("Noiseless", "AD") & fr
     scale_colour_manual("", values = c(COLOURS.LIST[1],COLOURS.LIST[4])) +
     scale_shape_manual("", values = c(19, 15)) +
     theme_paper() +
-    scale_x_continuous(ifelse(use_tikz, "$c_{\\boldsymbol{\\omega}}(\\boldsymbol{\\theta})$", "c"), limits = c(-0.3, 0.3)) +
+    scale_x_continuous(ifelse(use_tikz, "$c_{\\boldsymbol{0}}(\\boldsymbol{\\theta})$", "c"), limits = c(-0.3, 0.3)) +
     scale_y_discrete("", breaks = c("Noiseless", "AD")) +
     theme(
         legend.margin = margin(b = -4, t = 0),
