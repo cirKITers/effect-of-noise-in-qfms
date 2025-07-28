55544f25ed90b80cf849a2c80e1c43bb5766bcb2
majafranz <majafranz99@gmail.com>
2025-07-28 23:16:03 +0200

    cache intermediate training data

--- plotting/rplots/plot_training.r
+++ plotting/rplots/plot_training.r
@@ -20,116 +20,128 @@ if (length(args) == 1) {
 }
 
 data_path <- "csv_data/training.csv"
+data_path_processed <- "csv_data/training_processed.csv"
+data_path_summarised <- "csv_data/training_summarised.csv"
+data_path_summarised_freq <- "csv_data/training_summarised_freq.csv"
 
-d <- read_csv(data_path)
+if (!file.exists(data_path_summarised) && !file.exists(data_path_summarised_freq) && !file.exists(data_path_processed)) {
+    d <- read_csv(data_path)
 
-d <- d %>%
-    pivot_longer(
-        c(
-            BitFlip, PhaseFlip, Depolarizing,
-            AmplitudeDamping, PhaseDamping,
-            StatePreparation, Measurement, GateError
-        ),
-        names_to = "noise_type", values_to = "noise_value"
-    ) %>%
-    filter(!is.na(noise_value))
+    d <- d %>%
+        pivot_longer(
+            c(
+                BitFlip, PhaseFlip, Depolarizing,
+                AmplitudeDamping, PhaseDamping,
+                StatePreparation, Measurement, GateError
+            ),
+            names_to = "noise_type", values_to = "noise_value"
+        ) %>%
+        filter(!is.na(noise_value))
 
 
-d$noise_type[d$noise_value == 0] <- "Noiseless"
+    d$noise_type[d$noise_value == 0] <- "Noiseless"
 
-d <- d %>%
-    distinct(noise_type, noise_value, ansatz, qubits, frequencies, step, problem_seed, seed, .keep_all = TRUE) %>%
-    mutate(
-        coeff_abs = sqrt(coeffs_real^2 + coeffs_imag^2),
-        abs_target = sqrt(target_coefficients_real^2 + target_coefficients_imag^2),
-        coeff_abs_dist = abs(coeff_abs - abs_target),
-        noise_category = ifelse(
-            noise_type %in% c("BitFlip", "PhaseFlip", "Depolarizing"),
-            "Decoherent Gate",
-            ifelse(
-                noise_type %in% c("StatePreparation", "Measurement"),
-                "SPAM",
+    d <- d %>%
+        distinct(noise_type, noise_value, ansatz, qubits, frequencies, step, problem_seed, seed, .keep_all = TRUE) %>%
+        mutate(
+            coeff_abs = sqrt(coeffs_real^2 + coeffs_imag^2),
+            abs_target = sqrt(target_coefficients_real^2 + target_coefficients_imag^2),
+            coeff_abs_dist = abs(coeff_abs - abs_target),
+            noise_category = ifelse(
+                noise_type %in% c("BitFlip", "PhaseFlip", "Depolarizing"),
+                "Decoherent Gate",
                 ifelse(
-                    noise_type %in% c("AmplitudeDamping", "PhaseDamping"),
-                    "Damping",
+                    noise_type %in% c("StatePreparation", "Measurement"),
+                    "SPAM",
                     ifelse(
-                        noise_type == "GateError",
-                        "Coh.",
-                        ""
+                        noise_type %in% c("AmplitudeDamping", "PhaseDamping"),
+                        "Damping",
+                        ifelse(
+                            noise_type == "GateError",
+                            "Coh.",
+                            ""
+                        )
                     )
                 )
-            )
+            ),
+            noise_value = round(noise_value, digits = 3),
+        )
+
+    d$noise_category <- factor(d$noise_category, levels = c("", "Decoherent Gate", "SPAM", "Damping", "Coh."))
+    d$noise_type <- factor(d$noise_type,
+        levels = c(
+            "Noiseless", "BitFlip", "PhaseFlip", "Depolarizing",
+            "AmplitudeDamping", "PhaseDamping",
+            "StatePreparation", "Measurement",
+            "GateError"
         ),
-        noise_value = round(noise_value, digits = 3),
+        labels = c(
+            "Noiseless", "BF", "PF", "DP",
+            "AD", "PD",
+            "SP", "ME",
+            "CGE"
+        ),
     )
 
-d$noise_category <- factor(d$noise_category, levels = c("", "Decoherent Gate", "SPAM", "Damping", "Coh."))
-d$noise_type <- factor(d$noise_type,
-    levels = c(
-        "Noiseless", "BitFlip", "PhaseFlip", "Depolarizing",
-        "AmplitudeDamping", "PhaseDamping",
-        "StatePreparation", "Measurement",
-        "GateError"
-    ),
-    labels = c(
-        "Noiseless", "BF", "PF", "DP",
-        "AD", "PD",
-        "SP", "ME",
-        "CGE"
-    ),
-)
-
-d$ansatz <- factor(d$ansatz,
-    levels = c("Strongly_Entangling", "Hardware_Efficient", "Circuit_15", "Circuit_19"),
-    labels = c("SEA", "HEA", "Circuit 15", "Circuit 19")
-)
-
-
-
-d_summarised_freq <- d %>%
-    group_by(noise_category, noise_type, ansatz, qubits, step, frequencies) %>%
-    summarise(
-        mean_abs_dist = mean(coeff_abs_dist),
-        sd_abs_dist = sd(coeff_abs_dist),
-    ) %>%
-    mutate(
-        dist_lower_bound = mean_abs_dist - sd_abs_dist,
-        dist_upper_bound = mean_abs_dist + sd_abs_dist,
+    d$ansatz <- factor(d$ansatz,
+        levels = c("Strongly_Entangling", "Hardware_Efficient", "Circuit_15", "Circuit_19"),
+        labels = c("SEA", "HEA", "Circuit 15", "Circuit 19")
     )
 
 
-d <- d %>%
-    group_by(noise_category, noise_type, noise_value, ansatz, qubits, frequencies, step, problem_seed) %>%
-    summarise(
-        abs_target = mean(abs_target),
-        mean_coeff_abs = mean(coeff_abs),
-        sd_coeff_abs = sd(coeff_abs),
-        mean_abs_dist = mean(coeff_abs_dist),
-        sd_abs_dist = sd(coeff_abs_dist),
-        mean_mse = mean(mse),
-        sd_mse = sd(mse),
-        mean_dist = mean(coeff_dist),
-        sd_dist = sd(coeff_dist),
-    ) %>%
-    mutate(
-        coeff_lower_bound = mean_coeff_abs - sd_coeff_abs,
-        coeff_upper_bound = mean_coeff_abs + sd_coeff_abs,
-    )
 
-d_summarised <- d %>%
-    group_by(noise_category, noise_type, ansatz, qubits, step) %>%
-    summarise(
-        mean_mse = mean(mean_mse),
-        sd_mse = mean(sd_mse),
-        mean_dist = mean(mean_dist),
-        sd_dist = mean(sd_dist)
-    ) %>%
-    mutate(
-        mse_lower_bound = mean_mse - sd_mse,
-        mse_upper_bound = mean_mse + sd_mse,
-        dist_lower_bound = mean_dist - sd_dist,
-        dist_upper_bound = mean_dist + sd_dist,
-    )
+    d_summarised_freq <- d %>%
+        group_by(noise_category, noise_type, ansatz, qubits, step, frequencies) %>%
+        summarise(
+            mean_abs_dist = mean(coeff_abs_dist),
+            sd_abs_dist = sd(coeff_abs_dist),
+        ) %>%
+        mutate(
+            dist_lower_bound = mean_abs_dist - sd_abs_dist,
+            dist_upper_bound = mean_abs_dist + sd_abs_dist,
+        )
+
+
+    d <- d %>%
+        group_by(noise_category, noise_type, noise_value, ansatz, qubits, frequencies, step, problem_seed) %>%
+        summarise(
+            abs_target = mean(abs_target),
+            mean_coeff_abs = mean(coeff_abs),
+            sd_coeff_abs = sd(coeff_abs),
+            mean_abs_dist = mean(coeff_abs_dist),
+            sd_abs_dist = sd(coeff_abs_dist),
+            mean_mse = mean(mse),
+            sd_mse = sd(mse),
+            mean_dist = mean(coeff_dist),
+            sd_dist = sd(coeff_dist),
+        ) %>%
+        mutate(
+            coeff_lower_bound = mean_coeff_abs - sd_coeff_abs,
+            coeff_upper_bound = mean_coeff_abs + sd_coeff_abs,
+        )
+
+    d_summarised <- d %>%
+        group_by(noise_category, noise_type, ansatz, qubits, step) %>%
+        summarise(
+            mean_mse = mean(mean_mse),
+            sd_mse = mean(sd_mse),
+            mean_dist = mean(mean_dist),
+            sd_dist = mean(sd_dist)
+        ) %>%
+        mutate(
+            mse_lower_bound = mean_mse - sd_mse,
+            mse_upper_bound = mean_mse + sd_mse,
+            dist_lower_bound = mean_dist - sd_dist,
+            dist_upper_bound = mean_dist + sd_dist,
+        )
+    write_csv(d, data_path_processed)
+    write_csv(d_summarised, data_path_summarised)
+    write_csv(d_summarised_freq, data_path_summarised_freq)
+} else {
+    d <- read_csv(data_path_processed)
+    d_summarised <- read_csv(data_path_summarised)
+    d_summarised_freq <- read_csv(data_path_summarised_freq)
+}
 
 g <- ggplot(
     d_summarised_freq,
